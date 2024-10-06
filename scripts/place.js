d715d6202b369b5111514d0050cbd82c965bbaec
Evan <evan@evanalexander.dev>
2024-10-05 21:54:12 -0600

    Week 3: Country Page Updates

--- scripts/place.js
+++ scripts/place.js
@@ -4,16 +4,7 @@ const currentTempF = 43;
 const currentWindSpeedMph = 5;
 
 // Code created with the help of GitHub Copilot
-function calculateWindChill(tempF, windSpeedMph) {
-    if (tempF <= 50 && windSpeedMph > 3) {
-        const windChill = 35.74 + (0.6215 * tempF) 
-                        - (35.75 * Math.pow(windSpeedMph, 0.16)) 
-                        + (0.4275 * tempF * Math.pow(windSpeedMph, 0.16));
-        
-        return `${String(windChill.toFixed(2))} ºF`;
-    } else {
-        return "N/A";
-    }
-}
+const calculateWindChill = (tempF, windSpeedMph) => tempF <= 50 && windSpeedMph > 3 ? `${String((35.74 + 0.6215 * tempF - 35.75 * Math.pow(windSpeedMph, 0.16) + 0.4275 * tempF * Math.pow(windSpeedMph, 0.16)).toFixed(2))} ºF` : "N/A";
+
 
 windChill.innerHTML = calculateWindChill(currentTempF, currentWindSpeedMph);
